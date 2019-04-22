c20ff5c5eb63b824e3d92e33f381aaee807e51de
phanthienhoang95 <phanthienhoang95@gmail.com>
2019-04-22 08:17:11 +0700

    .

---
+++ string/script.js
@@ -0,0 +1,12 @@
+console.log('ok')
+function isFirstLetterUpperCase(str){
+    regexp = /^[A-Z]/;
+    if (regexp.test(str)) {
+       console.log("String's first character is uppercase");
+    } else {
+       console.log("String's first character is not uppercase");
+    }
+ }
+ isFirstLetterUpperCase('Abcd');
+ isFirstLetterUpperCase('abcd');
+ 
